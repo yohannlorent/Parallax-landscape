98f8a7cba3ad68ff34a6cddbebf842ce465678ab
Lorent Yohann <lorent.yohann@gmail.com>
2018-11-05 16:56:29 +0100

    finition navigation

--- _Wordpress/slider-landing-parallax.php
+++ _Wordpress/slider-landing-parallax.php
@@ -16,8 +16,6 @@ get_header();
 	
 </div>
 
-<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/right-arrow.svg" id="right-arrow"/>
-<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/left-arrow.svg" id="left-arrow"/>
 
 
 <style type="text/css">
@@ -31,28 +29,7 @@ get_header();
 		background-color:#000;
 		overflow:hidden;
 	}
-	 #right-arrow{
-      position:absolute;
-      z-index:801;
-      right:20px;
-      top:50%;
-      width:60px;
-      height:auto;
-      -webkit-transform: translateY(-50%);
-       -ms-transform: translateY(-50%);
-       transform: translateY(-50%);
-	   }
-	   #left-arrow{
-		  position:absolute;
-		  z-index:800;
-		  left:20px;
-		  top:50%;
-		  width:60px;
-		  height:auto;
-		  -webkit-transform: translateY(-50%);
-		   -ms-transform: translateY(-50%);
-		   transform: translateY(-50%);
-	   }
+
 	
 </style>
 
@@ -79,61 +56,18 @@ get_header();
  		window.bg=["01-bg-01.jpg","02-bg-01.jpg","03-bg-01.jpg"];
  		window.middle=["01-bg-02.png","02-bg-02.png","03-bg-02.png"];
  		window.front=["01-bg-03.png","02-bg-03.png","03-bg-03.png"];
+	   	window.titre=["Mountains","Holidays","Asia"];
+	   	window.chapeau=["A mountain is a large landform that rises above the surrounding land in a limited area, usually in the form of a peak. A mountain is generally steeper than a hill. Mountains are formed through tectonic forces or volcanism.","A holiday is a day set aside by custom or by law on which normal activities, especially business or work including school, are suspended or reduced. ","Asia is Earth's largest and most populous continent, located primarily in the Eastern and Northern Hemispheres."];
+	   	window.bouton=["WATCH MORE","WATCH MORE","WATCH MORE"];
 
  		//On appelle la fonction de création de slide
  		creaSlide();
-
- 		  //INIT -- lorsqu'on clique sur le bouton suivant
-         $( "#right-arrow" ).click(function() {
-            //on décrémente le z-index
-            zindex--;
-            //on incrémentde l'index / si on est à al fin du tableau on revient au début
-            i++;
-            if (i==bg.length){
-               i=0;
-            }
-            //on indique el sens d'animation
-            sensSlide=1;
-            //on appelle la fonction qui ajoute une nouvelle slide
-            creaSlide();
-            //on incrémente var init pour dire qu'on est plus sur al première slide on qu'on peut donc faire des animations
-            varInit++;
-
-             // on supprime le timer
-            clearInterval(timer);
-            //on supprime la progression de la progress bar
-            clearInterval(progressionProgresseBar);
-            //on supprime la progress bar
-            $('.fondTimer').remove();
-            
-         //INIT - Fin d'action bouton suivant
-         });
-         //INIT -- lorsqu'on clique sur le bouton precedent
-         $( "#left-arrow" ).click(function() {
-            //on décrémente le z-index
-            zindex--;
-            //on incrémentde l'index / si on est à al fin du tableau on revient au début
-            i--;
-            if (i<0){
-               i=bg.length-1;
-            }
-             //on indique el sens d'animation
-            sensSlide=0;
-            //on appelle la fonction qui ajoute une nouvelle slide
-            creaSlide();
-
-            // on supprime le timer
-            clearInterval(timer);
-            //on supprime la progression de la progress bar
-            clearInterval(progressionProgresseBar);
-            //on supprime la progress bar
-            $('.fondTimer').remove();
-
-            
-         
-         //INIT - Fin d'action bouton suivant 
-         });
-
+	   
+	 
+	   
+	   	///////////////////////////////////
+	    //   TIMER CHANGEMENT SLIDES     //
+		///////////////////////////////////
          // INIT - on change de slide toute les 5 secondes
          var timer =setInterval(function(){ 
 
@@ -151,18 +85,24 @@ get_header();
             //on incrémente var init pour dire qu'on est plus sur al première slide on qu'on peut donc faire des animations
             varInit++;
 
-            //On initialise la variable servant à calculer le pourcentage de progression de la progress bar
-            p=0;
-
+            //On fait disparaitre la progressbar pour al faire reaparaitre lorsque l'animationd e changement de slide sera finie
+			 p=0;
+			 $('.fondTimer').css('display','none');
+			  
+  			
          }, 5000);
 
+	   ///////////////////////////////////
+	   //           PROGRESS BAR        //
+	   ///////////////////////////////////
+	   
          //on crée un conteneur pour al progressbar auquel on pourra mettre la taille et la positionq ue l'on veut
         $('body').append("<div class='fondTimer'></div>");
 	  	$('.fondTimer').css('position','absolute');
 	  	$('.fondTimer').css('z-index','950');
-	  	$('.fondTimer').css('bottom','20px');
-	  	$('.fondTimer').css('left','20px');
-	  	$('.fondTimer').css('width','30%');
+	  	$('.fondTimer').css('bottom','0');
+	  	$('.fondTimer').css('left','0');
+	  	$('.fondTimer').css('width','100%');
 	  	$('.fondTimer').css('height','5px');
 
 	  	//on crée un conteneur à l'interieur ayant une position relative, qui permettra de mettr eles progress bar en absolute
@@ -180,7 +120,7 @@ get_header();
 	  	$('.bgProgessbar').css('width','100%');
 	  	$('.bgProgessbar').css('height','100%');
 	  	$('.bgProgessbar').css('background-color','#fff');
-	  	$('.bgProgessbar').css('opacity', '0.6');
+	  	$('.bgProgessbar').css('opacity', '0');
 
 	  	//On crée la progress bar
 	  	$('.contentProgressBar').append("<div class='progessbar'></div>");
@@ -190,7 +130,7 @@ get_header();
 	  	$('.progessbar').css('left','0');
 	  	$('.progessbar').css('width','0%');
 	  	$('.progessbar').css('height','100%');
-	  	$('.progessbar').css('background-color','#fff');
+	  	$('.progessbar').css('background-color','#d44d2e');
 
 	  	
 
@@ -203,9 +143,254 @@ get_header();
             //on applique le pourcentage à la progress bar
             $('.progessbar').css('width',pourcentage+'%');
          }, 50);
+	   
+	   ///////////////////////////////////
+	   //          BULLET POINTS        //
+	   ///////////////////////////////////
+	   
+	   
+	    //on crée le conteneur des bullet points
+		$('body').append("<div class='conteneur-bullet-points'></div>");
+	    //on y applique son style
+	   $('.conteneur-bullet-points').css('position','absolute');
+	   $('.conteneur-bullet-points').css('bottom','20px');
+	   $('.conteneur-bullet-points').css('left','50%');
+	   $('.conteneur-bullet-points').css('z-index','990');
+	   $('.conteneur-bullet-points').css('-webkit-transform','translateX(-50%)');
+	   $('.conteneur-bullet-points').css('-ms-transform','translateX(-50%)');
+	   $('.conteneur-bullet-points').css('transform','translateX(-50%)');
+	   
+	
+	   
+	   //creation de la flèche gauche
+	   $('.conteneur-bullet-points').append("<div class='left'></div>");
+	   $('.left').css('width','17px');
+	   $('.left').css('height','28px');
+	   $('.left').css('float','left');
+	   $('.left').css('position','relative');
+	   $('.left').css('margin-top','3px');
+	   $('.left').css('margin-right','20px');
+	   
+	   $('.left').append("<div class='left-barre-haut'></div>");
+	   $('.left-barre-haut').css('position','absolute');
+	   $('.left-barre-haut').css('right','0');
+	   $('.left-barre-haut').css('top','8px');
+	   $('.left-barre-haut').css('width','100%');
+	   $('.left-barre-haut').css('height','3px');
+	   $('.left-barre-haut').css('background-color','#fff');
+	   $('.left-barre-haut').css('border-radius','3px');
+	   $('.left-barre-haut').css('transform','rotate(-45deg)');
+	   
+	    $('.left').append("<div class='left-barre-bas'></div>");
+	   $('.left-barre-bas').css('position','absolute');
+	   $('.left-barre-bas').css('right','0');
+	   $('.left-barre-bas').css('top','18px');
+	   $('.left-barre-bas').css('width','100%');
+	   $('.left-barre-bas').css('height','3px');
+	   $('.left-barre-bas').css('background-color','#fff');
+	   $('.left-barre-bas').css('border-radius','3px');
+	   $('.left-barre-bas').css('transform','rotate(45deg)');
+	   
+	   //Au rollover
+		$('.left').mouseover(function() {
+			
+			$('.left-barre-haut').css('background-color','#d44d2e');
+			$('.left-barre-bas').css('background-color','#d44d2e');
+			$(this).css('cursor', 'pointer');
+		});
+			
+		//Au rollout
+		$('.left').mouseout(function() {
+		
+			$('.left-barre-haut').css('background-color','#fff');
+			$('.left-barre-bas').css('background-color','#fff');
+		});
+	   
+	   
+	   
+         //INIT -- lorsqu'on clique sur le bouton precedent
+        	$('.left').click(function() {
+            //on décrémente le z-index
+            zindex--;
+            //on incrémentde l'index / si on est à al fin du tableau on revient au début
+            i--;
+            if (i<0){
+               i=bg.length-1;
+            }
+             //on indique el sens d'animation
+            sensSlide=0;
+            //on appelle la fonction qui ajoute une nouvelle slide
+            creaSlide();
+ 			//on incrémente var init pour dire qu'on est plus sur al première slide on qu'on peut donc faire des animations
+            varInit++;
+
+            // on supprime le timer
+            clearInterval(timer);
+            //on supprime la progression de la progress bar
+            clearInterval(progressionProgresseBar);
+            //on supprime la progress bar
+            $('.fondTimer').remove();
+
+            
+         
+         //INIT - Fin d'action bouton suivant 
+         });
+	   
+	   
+	   
+	   
+	   
+	   $('.bullet'+iter).css('width','16px');
+	   //tant qu'il y a des slides...
+	   	for(var iter = 0; iter < bg.length; iter++) {
+			
+			// on crée une bullet
+			$('.conteneur-bullet-points').append("<div class='bullet"+iter+" bullet' id='"+iter+"'></div>");
+			//on y applique un style
+			$('.bullet'+iter).css('width','16px');
+			$('.bullet'+iter).css('height','16px');
+			$('.bullet'+iter).css('border-radius','16px');
+			$('.bullet'+iter).css('background-color','#fff');
+			$('.bullet'+iter).css('margin','10px');
+			$('.bullet'+iter).css('float','left');
+			
+			//Au rollover
+			$('.bullet'+iter).mouseover(function() {
+				//si l'id de la bullet est egal à la slide actuelle
+				if( $(this).attr("id") !=i){
+					 $(this).animate({
+		         		backgroundColor: '#d44d2e'
+		        	},"800", function() { });
+					$(this).css('cursor', 'pointer');
+				}
+			});
+			
+			//Au rollout
+		   $('.bullet'+iter).mouseout(function() {
+			   //si l'id de la bullet est egal à la slide actuelle
+			   if( $(this).attr("id") ==i){
+				   
+					$(this).css('background-color','#d44d2e');
+				}else{
+				//sinon
+					 $(this).animate({
+		         		backgroundColor: '#fff'
+		        	},"800", function() { });
+				}
+			});
+			
+			//Auclick
+			$('.bullet'+iter).click(function() {
+				
+				//on détermine le sens d'animation
+				if($(this).attr("id")<i){
+					sensSlide=0;
+
+				}else if($(this).attr("id")>i){
+					sensSlide=1;
+				}
+				
+				//Si on est pas sur la slide actuelle
+				if($(this).attr("id") !=i){
+					
+					//on met l'identation à la meme valeur que l'id de la bullet
+					i=$(this).attr("id");	
+					//on décrémente le z-index
+					zindex--;
+					//on appelle la fonction qui ajoute une nouvelle slide
+					creaSlide();
+					//on incrémente var init pour dire qu'on est plus sur al première slide on qu'on peut donc faire des animations
+					varInit++;
+					// on supprime le timer
+					clearInterval(timer);
+					//on supprime la progression de la progress bar
+					clearInterval(progressionProgresseBar);
+					//On fait disparaitre la progressbar pour al faire reaparaitre lorsque l'animationd e changement de slide sera finie
+					p=0;
+					$('.fondTimer').css('display','none');
+
+				}
+
+			});
+
+		}
+	    
+	    //creation de la flèche droite
+	   $('.conteneur-bullet-points').append("<div class='droite'></div>");
+	   $('.droite').css('width','17px');
+	   $('.droite').css('height','28px');
+	   $('.droite').css('float','left');
+	   $('.droite').css('position','relative');
+	   $('.droite').css('margin-top','3px');
+	   $('.droite').css('margin-left','20px');
+	   
+	   $('.droite').append("<div class='droite-barre-haut'></div>");
+	   $('.droite-barre-haut').css('position','absolute');
+	   $('.droite-barre-haut').css('right','0');
+	   $('.droite-barre-haut').css('top','8px');
+	   $('.droite-barre-haut').css('width','100%');
+	   $('.droite-barre-haut').css('height','3px');
+	   $('.droite-barre-haut').css('background-color','#fff');
+	   $('.droite-barre-haut').css('border-radius','3px');
+	   $('.droite-barre-haut').css('transform','rotate(45deg)');
+	   
+	    $('.droite').append("<div class='droite-barre-bas'></div>");
+	   $('.droite-barre-bas').css('position','absolute');
+	   $('.droite-barre-bas').css('right','0');
+	   $('.droite-barre-bas').css('top','18px');
+	   $('.droite-barre-bas').css('width','100%');
+	   $('.droite-barre-bas').css('height','3px');
+	   $('.droite-barre-bas').css('background-color','#fff');
+	   $('.droite-barre-bas').css('border-radius','3px');
+	   $('.droite-barre-bas').css('transform','rotate(-45deg)');
+	   
+		//Au rollover
+		$('.droite').mouseover(function() {
+			$('.droite-barre-haut').css('background-color','#d44d2e');
+			$('.droite-barre-bas').css('background-color','#d44d2e');
+			$(this).css('cursor', 'pointer');
+		});
+			
+		//Au rollout
+		$('.droite').mouseout(function() {
+			$('.droite-barre-haut').css('background-color','#fff');
+			$('.droite-barre-bas').css('background-color','#fff');
+		});
+	   
+	    //INIT -- lorsqu'on clique sur le bouton suivant
+         $('.droite').click(function() {
+            //on décrémente le z-index
+            zindex--;
+            //on incrémentde l'index / si on est à al fin du tableau on revient au début
+            i++;
+            if (i==bg.length){
+               i=0;
+            }
+            //on indique el sens d'animation
+            sensSlide=1;
+            //on appelle la fonction qui ajoute une nouvelle slide
+            creaSlide();
+            //on incrémente var init pour dire qu'on est plus sur al première slide on qu'on peut donc faire des animations
+            varInit++;
+            // on supprime le timer
+            clearInterval(timer);
+            //on supprime la progression de la progress bar
+            clearInterval(progressionProgresseBar);
+            //on supprime la progress bar
+            $('.fondTimer').remove();
+            
+         //INIT - Fin d'action bouton suivant
+         });
+	  
             
    });
 
+	
+	
+	
+	///////////////////////////////////
+	//      CHARGEMENT DS IMAGES     //
+	///////////////////////////////////
    	function creaSlide(){
 
    		 //INIT- on applique un preload aux images servant à créer la slide
@@ -218,10 +403,18 @@ get_header();
    	}
 	
 	
+	///////////////////////////////////
+	//            CREA SLIDE         //
+	///////////////////////////////////
 	function init_landscape(){
+		
+		$('.slider-parallax').append("<div class='text-parallax"+zindex+"'></div>");
 		
 		//on crée la slide
 		 $('.slider-parallax').append("<div class='slide-parallax"+zindex+"'></div>");
+		 $('.slide-parallax'+zindex).css('position','absolute');
+		 $('.slide-parallax'+zindex).css('z-index',zindex);
+		 $('.slide-parallax'+zindex).css('top','0');
 
 		//INIT -- on ajoute un style au slide de la valeur d'index
 		 $('.slide-parallax'+zindex).css('position','absolute');
@@ -314,6 +507,9 @@ get_header();
 		        },"slow", function() { 
 		        	//une fois sortie de l'écran on la supprime
 		        	 $('.slide-parallax'+(zindex+1)).remove();
+				   //on initialise la variable de calcul de la progression et on fait ré apparaitre la progress bar
+				    p=0;
+  					$('.fondTimer').css('display','block');
 
 		        });
  			//on fait apparaitre la slide nouvellement crée
@@ -322,25 +518,38 @@ get_header();
 		       },"slow", function() {});
 			}else if(sensSlide==0){
 				$('.slide-parallax'+zindex).css('left',-($(window).width()));
- 		 		
  		 		//on fait sortir la slide actuelle de l'écran
 		       $('.slide-parallax'+(zindex+1)).animate({
 		         	left:($(window).width())
 		        },"slow", function() { 
 		        	//une fois sortie de l'écran on la supprime
 		        	 $('.slide-parallax'+(zindex+1)).remove();
-
 		        });
  			//on fait apparaitre la slide nouvellement crée
 		      $('.slide-parallax'+(zindex)).animate({
               		left: 0
-		       },"slow", function() {});
+		       },"slow", function() { 
+			  });
 			}
 	   		
 	   	 }
+		
+		//on met a jour les bullets pour déterminer laquelle ne doit plus etre clickable
+		$.each($(".bullet") , function (k){
+			if( $(this).attr("id") ==i){
+				$(this).css('background-color','#d44d2e');
+			}else{
+				$(this).css('background-color','#fff');
+			}
+		});
 
+		
+		
 	}
 	
+	///////////////////////////////////////////////////////////////
+	//            FONCTION DE TAILLE DES IMAGES AUTOMATIQUES     //
+	///////////////////////////////////////////////////////////////
 	
 	function miseEnPlace(){
 
@@ -414,6 +623,9 @@ get_header();
    }
 
 
+	////////////////////////////////////////
+	//            PRELOAD IMAGES          //
+	////////////////////////////////////////
    //preload d'images
    var preloadPictures = function(pictureUrls, callback) {
     var i,
